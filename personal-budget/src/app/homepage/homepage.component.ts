f115b3a698f5e41d76e16024ba392337baaf2e51
Kieran Lorray <Kieranlorray@gmail.com>
2024-09-21 23:34:51 -0400

    Added Angular

---
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -0,0 +1,55 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Chart, ChartItem, registerables } from 'chart.js';
+Chart.register(...registerables)
+
+@Component({
+  selector: 'pb-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent implements OnInit {
+
+  public dataSource = {
+    datasets: [
+        {
+            data: [] as any,
+            backgroundColor: [
+                '#ffcd56',
+                '#ff6384',
+                '#36a2eb',
+                '#fd6b19',
+            ]
+        }
+    ],
+    labels: [] as any
+};
+
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:3000/budget')
+    .subscribe((res: any) => {
+
+      for (var i = 0; i < res.myBudget.length; i++) {
+        this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
+        this.dataSource.labels[i] = res.myBudget[i].title;
+        this.createChart();
+    }
+    });
+  }
+
+  createChart() {
+    //var ctx = document.getElementById('myChart').getContext('2d');
+    let chartStatus = Chart.getChart('myChart');
+    if (chartStatus != undefined) {
+      chartStatus.destroy();
+    }
+    var ctx = document.getElementById('myChart') as ChartItem;
+    var myPieChart = new Chart(ctx, {
+     type: 'pie',
+     data: this.dataSource
+    });
+}
+}
